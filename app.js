773cd116114ac71d6c6f6d77a6020db2130dbe8e
Caleb Lugo <cnl1211999@gmail.com>
2024-02-26 16:49:03 -0800

    feat: partial implementation of palyRound logic

--- app.js
+++ app.js
@@ -2,18 +2,20 @@
 // Gameboard factory
 function createGameboard (name, player1, player2) {
     const gameName = name;
-    const gameBoard = ['', '', '', '', '', '', '', '', ''];
-    const player1 = player1;
-    const player2 = player2;
+    let gameboard = ['', '', '',
+                       '', '', '',
+                       '', '', ''];
+    const gameboardPlayer1 = player1;
+    const gameboardPlayer2 = player2;
 
     let turn = 0;
     let gameWon = false;
     const getTurn = () => turn;
     const increaseTurn = () => turn++;
-    const getGameBoard = () => gameBoard;
+    const getGameboard = () => gameboard;
     const getPlayerInput = (input, playerMarker) => {
-        if(gameBoard[input] == '') {
-            gameBoard[input] = playerMarker;
+        if(gameboard[input] == '') {
+            gameboard[input] = playerMarker;
             increaseTurn();
         }
         else {
@@ -25,13 +27,19 @@ function createGameboard (name, player1, player2) {
         
     };
 
-    const playRound = (playerInput) => {
+    const playRound = () => {
         if(!gameWon) {
-            
+            // Player 1 goes on even turns, 
+            // player 2 on odd turns.
+            if(turn % 0 == 2) {
+                getPlayerInput(prompt("Please select an empty cell:" , gameboardPlayer1.getMarker()));
+            } else {
+                getPlayerInput(prompt("Please select an empty cell:" , gameboardPlayer2.getMarker()));
+            }
         }
     };
 
-    return {gameName, getTurn, increaseTurn, getGameBoard, getPlayerInput, playRound};
+    return {gameName, getTurn, increaseTurn, getGameboard, getPlayerInput, playRound};
 };
 
 function createPlayer (name) {
